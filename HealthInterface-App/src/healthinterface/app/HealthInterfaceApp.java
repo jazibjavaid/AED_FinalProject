672db24c58a8e952c97de54749957d27aeaf7942
tyagishantanu <tyagi.sha@northeastern.edu>
2022-11-27 12:39:53 -0500

    Project changes

--- HealthInterface-App/src/healthinterface/app/HealthInterfaceApp.java
+++ HealthInterface-App/src/healthinterface/app/HealthInterfaceApp.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package fooddesert.app;
+package healthinterface.app;
 
 /**
  *
  * @author shantanutyagi
  */
-public class FoodDesertApp {
+public class HealthInterfaceApp {
 
     /**
      * @param args the command line arguments
