a525b4856f9215fac998e3a23cbd4f3a046af3b9
jazibjavaid <javaid.j@northeastern.edu>
2022-12-04 23:20:40 -0500

    admin add emp setup

--- HealthInterface-App/src/Business/Role/NurseRole.java
+++ HealthInterface-App/src/Business/Role/NurseRole.java
@@ -4,10 +4,20 @@
  */
 package Business.Role;
 
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+import ui.NurseRole.NurseWorkAreaJPanel;
+
 /**
  *
  * @author jazibjavaid
  */
-public class NurseRole {
-    
+public class NurseRole extends Role{
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount useraccount, Organization org, Enterprise enterprise, EcoSystem system) {
+        return new NurseWorkAreaJPanel(userProcessContainer,useraccount,org,enterprise,system);
+    }
 }
