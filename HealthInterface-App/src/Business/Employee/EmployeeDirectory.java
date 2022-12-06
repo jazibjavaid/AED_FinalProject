6eabf8c0891a81d3f5e3f39149476b8ad262056b
Chikateyukta <114361980+Chikateyukta@users.noreply.github.com>
2022-12-06 17:11:32 -0500

    added class employee dir

--- HealthInterface-App/src/Business/Employee/EmployeeDirectory.java
+++ HealthInterface-App/src/Business/Employee/EmployeeDirectory.java
@@ -4,10 +4,44 @@
  */
 package Business.Employee;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author jazibjavaid
  */
 public class EmployeeDirectory {
+   private ArrayList<Employee> empList;
+
+    public EmployeeDirectory() {
+    }
+
+    public ArrayList<Employee> getEmpList() {
+        if(empList == null){
+            empList = new ArrayList();
+        }
+        return empList;
+    }
     
+    public Employee createEmp(String name){
+        Employee emp= new Employee();
+        emp.setName(name);
+        empList.add(emp);
+        return emp;
+    }
+    
+     public void removeEmp(Employee e){
+    
+        empList.remove(e);
+      
+    }
+      public void removeEmpByName(String empName){
+        for(int i=0; i<empList.size(); i++){
+            if(empList.get(i).getName().equalsIgnoreCase(empName)){
+                empList.remove(empList.get(i));
+            }
+        }
+        
+      
+    }
 }
