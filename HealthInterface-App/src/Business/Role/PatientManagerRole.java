364de44cf72161f66cbb5f043861922401ee2a69
tyagishantanu <tyagi.sha@northeastern.edu>
2022-12-06 18:57:54 -0500

    PatientManager services and requests

--- HealthInterface-App/src/Business/Role/PatientManagerRole.java
+++ HealthInterface-App/src/Business/Role/PatientManagerRole.java
@@ -4,10 +4,22 @@
  */
 package Business.Role;
 
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+import UI.PatientManagerRole.PatientManagerWorkAreaJPanel;
+
 /**
  *
  * @author jazibjavaid
  */
-public class PatientManagerRole {
+public class PatientManagerRole extends Role{
+    
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount useraccount, Organization org, Enterprise enterprise, EcoSystem system) {
+        return new PatientManagerWorkAreaJPanel(userProcessContainer, useraccount, org, enterprise, system);
+    }
     
 }
