3727d701526abc96aee029decdc96dd7cf8c04af
tyagishantanu <tyagi.sha@northeastern.edu>
2022-12-06 17:24:10 -0500

    Patient support ent panel and classes

--- HealthInterface-App/src/Business/Role/PatientSupportEntAdminRole.java
+++ HealthInterface-App/src/Business/Role/PatientSupportEntAdminRole.java
@@ -4,10 +4,21 @@
  */
 package Business.Role;
 
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+
 /**
  *
  * @author jazibjavaid
  */
-public class PatientSupportEntAdminRole {
+public class PatientSupportEntAdminRole extends Role{
+    
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount useraccount, Organization org, Enterprise enterprise, EcoSystem system) {
+        return new PatientSupportAdminWorkAreaJPanel(userProcessContainer, useraccount, org, enterprise, system);
+    }
     
 }
