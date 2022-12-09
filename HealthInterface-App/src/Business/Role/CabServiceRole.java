ff6b26b59aed0d4c8b0bd5b4a6bc67a90721bc07
Chikateyukta <114361980+Chikateyukta@users.noreply.github.com>
2022-12-09 18:52:19 -0500

    added cab service role

--- HealthInterface-App/src/Business/Role/CabServiceRole.java
+++ HealthInterface-App/src/Business/Role/CabServiceRole.java
@@ -4,10 +4,22 @@
  */
 package Business.Role;
 
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import UI.CabServiceRole.CabServiceWorkAreaJPanel;
+import javax.swing.JPanel;
+
+
 /**
  *
  * @author jazibjavaid
  */
 public class CabServiceRole {
+    
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount useraccount, Organization org, Enterprise enterprise, EcoSystem system) {
+        return new CabServiceWorkAreaJPanel(userProcessContainer,useraccount,org,enterprise,system);
+    }
     
 }
